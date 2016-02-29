de6617bb1a23a84c5b64a10ea6e044c4a860c10d
Wessel <wessel.p.bruinsma@gmail.com>
2016-02-29 16:38:25 +0000

    Renamed arhc.py to squash.py

--- performance.py
+++ performance.py
@@ -17,8 +17,8 @@ class Test:
         self.bits = bits
         
     def run(self, args=[]):
-        pComp = Popen(['./arhc.py'] + args, stdin=PIPE, stdout=PIPE)
-        pDecomp = Popen(['./arhc.py', '--decompress'] + args, stdin=PIPE, stdout=PIPE)
+        pComp = Popen(['./squash.py'] + args, stdin=PIPE, stdout=PIPE)
+        pDecomp = Popen(['./squash.py', '--decompress'] + args, stdin=PIPE, stdout=PIPE)
         pComp.stdin.write(self.bits)
         outComp = pComp.stdout.read()
         pDecomp.stdin.write(outComp)
